ba0f4422360f3517a92c4c0974ec8f43a67adbc5
irohasuko <s18f1037@bene.fit.ac.jp>
2022-12-24 16:53:59 +0900

    flexbox対応

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,12 +4,18 @@ import styled from 'styled-components';
 const NewsBox = styled.div`
     text-align: center;
     border: solid 1px;
+    display: flex;
+    flex-wrap: wrap;
 `
 
 const News = () => {
     return (
         <NewsBox>
             <h2>ニュース一覧</h2>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
             <Card />
         </NewsBox>
     );
