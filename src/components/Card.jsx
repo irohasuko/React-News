9ba512dbcd5d05249b22ed0aae473a545c28f96f
irohasuko <s18f1037@bene.fit.ac.jp>
2022-12-24 17:01:15 +0900

    カード中身作成＆スタイル適用

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,34 @@
-const Card = () =>{
+import styled from "styled-components";
+
+const CardHeader = styled.div`
+    background-color: lightgray;
+    font-size: 20px;
+    border: solid 1px;
+    border-radius: 10px 10px 0px 0px;
+`
+const CardImg = styled.div`
+    border-right: solid 1px;
+    border-left: solid 1px;
+`
+
+const CardBody = styled.div`
+    font-size: 15px;
+    border: solid 1px;
+`
+
+const StyledCard = styled.div`
+    width: 30%;
+    margin: 5px;
+`
+
+const Card = () => {
     return (
-        <div>カード</div>
+        <StyledCard>
+            <CardHeader>ヘッダー</CardHeader>
+            <CardImg>画像</CardImg>
+            <CardBody>ニュース概要</CardBody>
+        </StyledCard>
     );
 };
-  
+
 export default Card;
