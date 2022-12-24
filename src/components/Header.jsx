5369c0d25db62fe45f4084e4d7512144c1af6535
irohasuko <s18f1037@bene.fit.ac.jp>
2022-12-24 14:20:19 +0900

    ヘッダーコンポーネントにCSS適用

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
-const Header = () =>{
+import styled from "styled-components";
+
+const HeaderText = styled.header`
+    font-size: 64px;
+    font-family: Arial Black;
+    color: #5f5f5f;
+    text-align: center;
+    padding-top: 10px;
+    padding-bottom; 10px;
+`
+const Header = () => {
     return (
-        <div>ヘッダー</div>
+        <HeaderText>MyNews</HeaderText>
     );
 };
-  
+
 export default Header;
