e898ce7239ebffad28dd272c77413c3517e57ac0
irohasuko <s18f1037@bene.fit.ac.jp>
2022-12-24 17:00:03 +0900

    フレックスボックスのコンテナ位置変更

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,19 +4,25 @@ import styled from 'styled-components';
 const NewsBox = styled.div`
     text-align: center;
     border: solid 1px;
+`
+
+const CardContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
+    justify-content: center;
 `
 
 const News = () => {
     return (
         <NewsBox>
             <h2>ニュース一覧</h2>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <CardContainer>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </CardContainer>
         </NewsBox>
     );
 };
