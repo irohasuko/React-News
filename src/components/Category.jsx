2d0e959bb0c1e9029db7f59142a475edf30edf3f
irohasuko <s18f1037@bene.fit.ac.jp>
2022-12-24 14:40:28 +0900

    カテゴリの枠組み完成

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,27 @@
+import styled from "styled-components";
+
+const Categorybox = styled.div`
+
+`
+
+const Selector = styled.select`
+
+`
+
 const Category = () => {
     return (
-        <div>カテゴリー</div>
+        <Categorybox>
+            <div>カテゴリでフィルター</div>
+            <select>
+                <option default>カテゴリを選択</option>
+                <option>テスト１</option>
+                <option>テスト２</option>
+                <option>テスト３</option>
+                <option>テスト４</option>
+            </select>
+            <button>検索</button>
+        </Categorybox>
     );
 };
-  
+
 export default Category;
