8e356d29f0afd6754dfc7e04869415033285821d
irohasuko <s18f1037@bene.fit.ac.jp>
2022-12-25 11:11:13 +0900

    カードのスタイル編集

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,22 @@
 import styled from "styled-components";
-import { useState } from "react";
 
 const CardHeader = styled.div`
     background-color: lightgray;
     font-size: 20px;
+    padding: 5%;
     border: solid 1px;
     border-radius: 10px 10px 0px 0px;
 `
 const CardImg = styled.img`
-    border-right: solid 1px;
-    border-left: solid 1px;
-    width: 100%;
+    border: solid 1px;
+    width: 99%;
 `
 
 const CardBody = styled.div`
     font-size: 15px;
+    padding: 5%;
     border: solid 1px;
+    margin-top: -10px;
 `
 
 const StyledCard = styled.div`
@@ -24,8 +25,7 @@ const StyledCard = styled.div`
 `
 
 const Card = ({ news }) => {
-    console.log(news.media);
-    const imgUrl = '0' in news.media ? news.media[0]["media-metadata"][2].url : '';
+    const imgUrl = '0' in news.media ? news.media[0]["media-metadata"][2].url : `${process.env.PUBLIC_URL}/logo192.png`;
     return (
         <StyledCard>
             <CardHeader>{news.title}</CardHeader>
