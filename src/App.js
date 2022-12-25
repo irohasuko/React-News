f70ab4c12e18c1c43935656739c4e426185d18ff
irohasuko <s18f1037@bene.fit.ac.jp>
2022-12-25 13:44:52 +0900

    カテゴリ変更用useStateの定義

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Category from './components/Category';
 import News from './components/News';
 
 function App() {
+  const [category, setCategory] = useState('home');
   return (
     <div className="App">
       <Header />
-      <Category />
-      <News />
+      <Category setCategory={setCategory}/>
+      <News category={category}/>
     </div>
   );
 }
