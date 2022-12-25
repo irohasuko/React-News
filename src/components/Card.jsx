27f856d061b2a8de0ab5bff5ae0e5fabd6233453
irohasuko <s18f1037@bene.fit.ac.jp>
2022-12-25 10:57:48 +0900

    APIから取得したデータを表示

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 const CardHeader = styled.div`
     background-color: lightgray;
@@ -6,9 +7,10 @@ const CardHeader = styled.div`
     border: solid 1px;
     border-radius: 10px 10px 0px 0px;
 `
-const CardImg = styled.div`
+const CardImg = styled.img`
     border-right: solid 1px;
     border-left: solid 1px;
+    width: 100%;
 `
 
 const CardBody = styled.div`
@@ -21,12 +23,14 @@ const StyledCard = styled.div`
     margin: 5px;
 `
 
-const Card = () => {
+const Card = ({ news }) => {
+    console.log(news.media);
+    const imgUrl = '0' in news.media ? news.media[0]["media-metadata"][2].url : '';
     return (
         <StyledCard>
-            <CardHeader>ヘッダー</CardHeader>
-            <CardImg>画像</CardImg>
-            <CardBody>ニュース概要</CardBody>
+            <CardHeader>{news.title}</CardHeader>
+            <CardImg src={imgUrl}></CardImg>
+            <CardBody>{news.abstract}</CardBody>
         </StyledCard>
     );
 };
